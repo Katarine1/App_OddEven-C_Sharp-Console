e934f8bc94d9ea80a8dfa338433a6561d975bf80
Katarine1 <kba.2879@gmail.com>
2019-12-28 22:20:16 -0300

    App Odd or Even with C# in Console

---
+++ App1_OddEven/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace App1_OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.Write("\t\tEnter a number : ");
+            long n = long.Parse(Console.ReadLine());
+
+            if(n % 2 == 0)
+            {
+                Console.WriteLine("\t\t-> Even number");
+            }
+            else
+            {
+                Console.WriteLine("\t\t-> Odd number\n");
+            }
+        }
+    }
+}
